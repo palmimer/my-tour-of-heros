5bb5fb460e9dcf1712bfc2f5081a39fd1a8cc972
Palmai Mercedesz <mercedesz.palmai@dextraline.hu>
2020-10-23 23:44:39 +0200

    routing + dashboard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
+  // This route redirects a URL that fully matches the empty path to the route whose path is '/dashboard'.
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
-
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      // returns the sliced list of heroes at positions 1 to 5
+      heroes => this.heroes = heroes.slice(1, 5)
+    );
+  }
+
+}
